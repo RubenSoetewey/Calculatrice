ec9c43f4b9b8be4b329fc62683665530ce303b82
Ruben <ruben.desert@eemi.fr>
2019-01-16 09:08:17 +0100

    add fragment to mainLayout

---
+++ app/src/main/java/com/example/rubyesgi/tpcalculatrice/PadFragment.java
@@ -0,0 +1,11 @@
+package com.example.rubyesgi.tpcalculatrice;
+
+import android.support.v4.app.Fragment;
+
+public class PadFragment extends Fragment {
+
+    public PadFragment() {
+        // Required empty public constructor
+    }
+
+}
